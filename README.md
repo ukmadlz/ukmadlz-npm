e029472e4d7d63c316e8bc3ca7e7893a8f9be297
Bradley Holt <bradley.holt@us.ibm.com>
2016-03-17 17:31:25 -0400

    Create module

---
+++ README.md
@@ -0,0 +1,15 @@
+```
+$ npm install -g bradley
+$ bradley
+
+Bradley Holt is a Developer Advocate with IBM (opinions and views are his own)
+
+Bradley writes and speaks about topics such as CouchDB, PouchDB, offline-first applications, PHP, Node.js, and Domain-Driven Design.
+
+Blog: <https://ibm.biz/bradley-holt>
+Twitter: <https://twitter.com/BradleyHolt>
+GitHub: <https://www.github.com/bradley-holt>
+LinkedIn: <https://www.linkedin.com/in/bradleyholt>
+Speaker Deck: <https://speakerdeck.com/bradleyholt>
+Stack Overlow: <http://stackoverflow.com/users/4847080/bradley-holt>
+```
---
+++ index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+console.log("");
+console.log("Bradley Holt is a Developer Advocate with IBM (opinions and views are his own)");
+console.log("");
+console.log("Bradley writes and speaks about topics such as CouchDB, PouchDB, offline-first applications, PHP, Node.js, and Domain-Driven Design.");
+console.log("");
+console.log("Blog: <https://ibm.biz/bradley-holt>");
+console.log("Twitter: <https://twitter.com/BradleyHolt>");
+console.log("GitHub: <https://www.github.com/bradley-holt>");
+console.log("LinkedIn: <https://www.linkedin.com/in/bradleyholt>");
+console.log("Speaker Deck: <https://speakerdeck.com/bradleyholt>");
+console.log("Stack Overlow: <http://stackoverflow.com/users/4847080/bradley-holt>");
