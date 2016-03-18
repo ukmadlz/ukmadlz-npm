ee1baab950bb6b1f0329b8a6db86ae3d1d752bd1
Mike Elsmore <mike@elsmore.me>
2016-03-18 08:17:22 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 #!/usr/bin/env node
 
 console.log("");
-console.log("Bradley Holt is a Developer Advocate with IBM (opinions and views are his own)");
+console.log("Mike Elsmore is a Developer Advocate with IBM (opinions and views are his own)");
 console.log("");
-console.log("Bradley writes and speaks about topics such as CouchDB, PouchDB, offline-first applications, PHP, Node.js, and Domain-Driven Design.");
+console.log("Mike hacks and speaks about topics such as CouchDB, PouchDB, IBM Graph, offline-first applications, PHP and Node.js.");
 console.log("");
-console.log("Blog: <https://ibm.biz/bradley-holt>");
-console.log("Twitter: <https://twitter.com/BradleyHolt>");
-console.log("GitHub: <https://www.github.com/bradley-holt>");
-console.log("LinkedIn: <https://www.linkedin.com/in/bradleyholt>");
-console.log("Speaker Deck: <https://speakerdeck.com/bradleyholt>");
-console.log("Stack Overlow: <http://stackoverflow.com/users/4847080/bradley-holt>");
+console.log("Blog: <https://elsmore.me>");
+console.log("Twitter: <https://twitter.com/ukmadlz>");
+console.log("GitHub: <https://www.github.com/ukmadlz>");
+console.log("LinkedIn: <https://uk.linkedin.com/in/mikeelsmore>");
