c445333c3d3dbb66a84ff564351f041e0b81a79b
Mike Elsmore <mike.elsmore@uk.ibm.com>
2017-03-13 20:06:05 +0000

    Update for the job

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
 console.log('');
-console.log('Mike Elsmore is a Developer Advocate with IBM (opinions and views are his own)');
+console.log('Mike Elsmore is a freelance developer and developer relations consultant.');
 console.log('');
 console.log('Mike hacks and speaks about topics such as CouchDB, PouchDB, IBM Graph, offline-first applications, PHP and Node.js.');
 console.log('');
 console.log('E-mail: <mike@elsmore.me>');
-console.log('Blog: <http://elsmore.me>');
+console.log('Blog: <https://elsmore.me>');
 console.log('Twitter: <https://twitter.com/ukmadlz>');
 console.log('GitHub: <https://www.github.com/ukmadlz>');
 console.log('LinkedIn: <https://uk.linkedin.com/in/mikeelsmore>');
