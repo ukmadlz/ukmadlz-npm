4f2d69e8ea8588161a6d22babb424f7d0584fb0b
Mike Elsmore <mike@elsmore.me>
2021-05-19 17:38:30 +0100

    :art: Update the bio and change the nasty way I displayed it

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 #!/usr/bin/env node
 
-console.log('');
-console.log('Mike Elsmore is a freelance developer and developer relations consultant.');
-console.log('');
-console.log('Mike hacks and speaks about topics such as CouchDB, PouchDB, IBM Graph, offline-first applications, PHP and Node.js.');
-console.log('');
-console.log('E-mail: <mike@elsmore.me>');
-console.log('Blog: <https://elsmore.me>');
-console.log('Twitter: <https://twitter.com/ukmadlz>');
-console.log('GitHub: <https://www.github.com/ukmadlz>');
-console.log('LinkedIn: <https://uk.linkedin.com/in/mikeelsmore>');
+const package = require('./package.json');
+
+const theMeBit = `
+Mike Elsmore works for Optic Labs as a Developer Advocate.
+
+Mike hacks and speaks about topics such as ${package.keywords.join(', ')}.
+
+E-mail: <mike@elsmore.me>
+Website: <https://elsmore.me>
+Work E-mail: <mike.elsmore@useoptic.com>
+Twitch: <https://twitch.tv/ukmadlz>
+Twitter: <https://twitter.com/ukmadlz>
+GitHub: <https://www.github.com/ukmadlz>
+LinkedIn: <https://uk.linkedin.com/in/mikeelsmore>
+`
+console.log(theMeBit)
