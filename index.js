b038f39fd76bbd2c27a81a1d94debd622393de75
Mike Elsmore <mike@elsmore.me>
2022-04-29 18:13:11 +0100

    :sparkles: Use the elsmore.me api

--- index.js
+++ index.js
@@ -2,17 +2,21 @@
 
 const package = require('./package.json');
 
-const theMeBit = `
-Mike Elsmore works for Infosum as a Lead Developer Advocate.
+const API_URL = 'http://api.elsmore.me/socials';
 
-Mike hacks and speaks about topics such as ${package.keywords.join(', ')}.
-
-E-mail: <mike@elsmore.me>
-Website: <https://elsmore.me>
-Work E-mail: <mike.elsmore@infosum.com>
-Twitch: <https://twitch.tv/ukmadlz>
-Twitter: <https://twitter.com/ukmadlz>
-GitHub: <https://www.github.com/ukmadlz>
-LinkedIn: <https://uk.linkedin.com/in/mikeelsmore>
-`
-console.log(theMeBit)
+fetch(API_URL)
+.then((res) => res.json())
+.then((data) => {
+    console.log(data)
+    const theMeBit = `
+    Mike Elsmore works for Infosum as a Lead Developer Advocate.
+    
+    Mike hacks and speaks about topics such as ${package.keywords.join(', ')}.
+    
+    E-mail: <mike@elsmore.me>
+    Website: <https://elsmore.me>
+    Work E-mail: <mike.elsmore@infosum.com>
+${data.data.map(d => `    ${d.name}: <${d.link}>`).join('\n')}
+    `
+    console.log(theMeBit)
+})
