0000f6470b3e507bdaefffa08e74eb3db4151978
Mike Elsmore <mike@elsmore.me>
2022-04-29 16:59:13 +0100

    :bento: New job and skillz

--- index.js
+++ index.js
@@ -3,13 +3,13 @@
 const package = require('./package.json');
 
 const theMeBit = `
-Mike Elsmore works for Optic Labs as a Developer Advocate.
+Mike Elsmore works for Infosum as a Lead Developer Advocate.
 
 Mike hacks and speaks about topics such as ${package.keywords.join(', ')}.
 
 E-mail: <mike@elsmore.me>
 Website: <https://elsmore.me>
-Work E-mail: <mike.elsmore@useoptic.com>
+Work E-mail: <mike.elsmore@infosum.com>
 Twitch: <https://twitch.tv/ukmadlz>
 Twitter: <https://twitter.com/ukmadlz>
 GitHub: <https://www.github.com/ukmadlz>
