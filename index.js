19265f3ec774837dfddebd3ca984859e236be307
Mike Elsmore <mike@elsmore.me>
2023-05-26 16:46:12 +0100

    :poop: Added a decent description of my day job, and forced the node version to be 18+

--- index.js
+++ index.js
@@ -8,13 +8,15 @@ fetch(API_URL)
 .then((res) => res.json())
 .then((data) => {
     const theMeBit = `
-    Mike Elsmore works for Infosum as a Lead Developer Advocate.
+    Mike Elsmore works for Infobip as a Senior Developer Advocate.
+
+    For a day job he's usually talking about stuff, building stuff (occasionally SDKs), and trying share stuff.
     
     Mike hacks and speaks about topics such as ${package.keywords.join(', ')}.
     
     E-mail: <mike@elsmore.me>
     Website: <https://elsmore.me>
-    Work E-mail: <mike.elsmore@infosum.com>
+    Work E-mail: <mike.elsmore@infobip.com>
 ${data.data.map(d => `    ${d.name}: <${d.link}>`).join('\n')}
     `
     console.log(theMeBit)
