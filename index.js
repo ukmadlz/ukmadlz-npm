649b5ba9bd2509a8532a8b5948d6dbbf3b521dc9
Mike Elsmore <mike@Mikes-MacBook-Pro.local>
2024-06-16 10:01:19 +0100

    Update for hire piece

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 #!/usr/bin/env node
 
-const package = require('./package.json');
+const package = require("./package.json");
 
-const API_URL = 'http://elsmore.me/api/socials';
+const API_URL = "http://elsmore.me/api/socials";
 
 fetch(API_URL)
-.then((res) => res.json())
-.then((data) => {
+  .then((res) => res.json())
+  .then((data) => {
     const theMeBit = `
-    Mike Elsmore works for Infobip as a Senior Developer Advocate.
+    Mike Elsmore is available for hire for software development, MVPs, Developer Experience and Developer Relations related work.
 
     For a day job he's usually talking about stuff, building stuff (occasionally SDKs), and trying share stuff.
-    
-    Mike hacks and speaks about topics such as ${package.keywords.join(', ')}.
-    
+
+    Mike hacks and speaks about topics such as ${package.keywords.join(", ")}.
+
     E-mail: <mike@elsmore.me>
     Website: <https://elsmore.me>
-    Work E-mail: <mike.elsmore@infobip.com>
-${data.data.map(d => `    ${d.name}: <${d.link}>`).join('\n')}
-    `
-    console.log(theMeBit)
-})
+${data.data.map((d) => `    ${d.name}: <${d.link}>`).join("\n")}
+    `;
+    console.log(theMeBit);
+  });
